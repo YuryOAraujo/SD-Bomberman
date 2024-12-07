86f56c0b89bc002b4f7d16657afcc515750b3adb
Igor Augusto <igoraugusto36s@gmail.com>
2024-12-06 22:48:05 -0300

    fix: arruma erro na declaração da classe

--- client/spritesheet.py
+++ client/spritesheet.py
@@ -1,10 +1,10 @@
 import pygame
 
-class SpriteSheet():
-	def __init__(self, image):
+class SpriteSheet:
+	def __init__(self, image) -> None:
 		self.sheet = image
 
-	def get_image(self, frame, y_offset, width, height, scale, colour):
+	def get_image(self, frame, y_offset, width, height, scale, colour) -> pygame.Surface:
 		image = pygame.Surface((width, height)).convert_alpha()
 		image.blit(self.sheet, (0, 0), ((frame * width), (height * y_offset), width, height))
 		image = pygame.transform.scale(image, (width * scale, height * scale))
